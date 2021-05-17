f4e063bac8ca57779465334b0c5a59fcac4528fc
Eva Rodriguez <maevarodriguez1@gmail.com>
2021-05-17 13:41:52 +0200

    Agrego ejercicio 1 y 2

---
+++ src/Ejercicio1.java
@@ -0,0 +1,19 @@
+public class Ejercicio1 {
+   /*
+        Ingrese por teclado dos valores enteros que uno sera base y otro altura que representaran la base y
+        la altura de un cuadrado. Calcular el area del cuadrado y mostrarla en pantalla con el mensaje:
+        "El cuadrado de base: X y altura: Y tiene un area de X"
+        Recuerda que para calcular el area la formula es = base * altura
+
+    */
+    public static void main(String args[]) {
+        // Aqui declaro las variables que voy a utilizar e ingreso por teclado si hace falta
+
+
+        // Aqui agrego la logica
+
+
+        // Aqui  muestro el resultado final
+
+    }
+}
---
+++ src/Ejercicio2.java
@@ -0,0 +1,19 @@
+public class Ejercicio2 {
+    /*
+         Ingrese por teclado dos valores enteros que uno sera base y otro altura que representaran la base y
+         la altura de un cuadrado. Calcular el perimetro del cuadrado y mostrarla en pantalla con el mensaje:
+         "El cuadrado de base: X y altura: Y tiene un perimetro de X"
+         Recuerda que para calcular el perimetro la formula es = base * 2 + altura * 2
+     */
+
+    public static void main(String args[]) {
+        // Aqui declaro las variables que voy a utilizar e ingreso por teclado si hace falta
+
+
+        // Aqui agrego la logica
+
+
+        // Aqui  muestro el resultado final
+
+    }
+}

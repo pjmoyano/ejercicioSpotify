9192114f30f4db836f85cba7735ef080a260584c
Eva Rodriguez <maevarodriguez1@gmail.com>
2021-05-17 15:14:39 +0200

    ejercicio 2 tambien resuelto, lo probe y funciona

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -4,19 +4,25 @@ public class Ejercicio2 {
     /*
          Ingrese por teclado dos valores enteros que uno sera base y otro altura que representaran la base y
          la altura de un cuadrado. Calcular el perimetro del cuadrado y mostrarla en pantalla con el mensaje:
-         "El cuadrado de base: X y altura: Y tiene un perimetro de X"
+         "X"
          Recuerda que para calcular el perimetro la formula es = base * 2 + altura * 2
      */
 
     public static void main(String args[]) {
         // Aqui declaro las variables que voy a utilizar e ingreso por teclado si hace falta
+        int base;
+        int altura;
 
+        System.out.println("Ingrese el primer numero: ");
+        base = ingresarNumero();
+        System.out.println("Ingrese el segundo numero: ");
+        altura = ingresarNumero();
 
         // Aqui agrego la logica
-
+        int perimetro = (base*2)+(altura*2);
 
         // Aqui  muestro el resultado final
-
+        System.out.println("El cuadrado de base:" +base+ " y altura:" +altura+ " tiene un perimetro de " +perimetro);
     }
 
     public static int ingresarNumero() {

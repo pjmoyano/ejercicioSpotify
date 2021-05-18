afcc8e182bfdc76d0d1c9877c23d090d28a42170
Eva Rodriguez <maevarodriguez1@gmail.com>
2021-05-18 15:02:26 +0200

    repositorio ejercicio 4, completado y funcionando, le agregue un else if por si era menor a 100 el resultdo de la suma, y le agregue mensaje "no haz superado el 100 por ciento"

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -10,12 +10,49 @@ public class Ejercicio4 {
 
     public static void main(String args[]) {
         // Aqui declaro las variables que voy a utilizar e ingreso por teclado si hace falta
-
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+        int g;
+        int h;
+        int i;
+        int j;
+        System.out.println("Ingrese el primer numero: ");
+        a = ingresarNumero();
+        System.out.println("Ingrese el segundo numero: ");
+        b = ingresarNumero();
+        System.out.println("Ingrese el primer numero: ");
+        c = ingresarNumero();
+        System.out.println("Ingrese el segundo numero: ");
+        d = ingresarNumero();
+        System.out.println("Ingrese el primer numero: ");
+        e = ingresarNumero();
+        System.out.println("Ingrese el segundo numero: ");
+        f = ingresarNumero();
+        System.out.println("Ingrese el primer numero: ");
+        g = ingresarNumero();
+        System.out.println("Ingrese el segundo numero: ");
+        h = ingresarNumero();
+        System.out.println("Ingrese el primer numero: ");
+        i = ingresarNumero();
+        System.out.println("Ingrese el segundo numero: ");
+        j = ingresarNumero();
 
         // Aqui agrego la logica
+        int resultadoSuma = (a+b+c+d+e+f+g+h+i+j);
+        if (resultadoSuma > 100) {
+            System.out.println("Haz superado el 100 por ciento");
+        }
+        else if (resultadoSuma < 100) {
+            System.out.println("No haz superado el 100 por ciento");
+        }
 
 
         // Aqui  muestro el resultado final
+
     }
 
     public static int ingresarNumero() {

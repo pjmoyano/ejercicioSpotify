d4c2886c43daa09bdce4342674e45391e7d7df7c
Eva Rodriguez <maevarodriguez1@gmail.com>
2021-05-24 13:38:30 +0200

    repositorio ejercicio 7, resuelto y funcionando aparentemente, le asigne el nombre de sumatoria final al resultado, prque factoreo es en el caso de multiplicar. quise agregarle un else, por si se ingresaba un numero <= 0 pero no me salió

--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -2,28 +2,39 @@ import java.util.Scanner;
 
 public class Ejercicio7 {
     /*
-         Ingresar por teclado un numero y calcular la suma de todos los numeros desde 0 hasta el numero ingresado.
-         (no me acuerdo el nombre de esto, creo que era factoreo)
-         Mostrar el mensaje "El factoreo de X es Y"
+        Ingresar por teclado un numero y calcular la suma de todos los numeros desde 0 hasta el numero ingresado.
+
+         Mostrar el mensaje "La sumatoria final de X es Y"
          Por ejemplo:
-         Ingresamos el numero 10.
+         Ejemplo 1) Ingresamos el numero 10.
          0+1+2+3+4+5+6+7+8+9+10 = 55
-         Resultado: "El factoreo de 10 es 55"
+         Resultado: "La sumatoria final de 10 es 55"
+         Ejemplo 2) Ingresamos el numero 3
+         0+1+2+3 = 6
+         Resultado: "La sumatoria final de 3 es 6"
      */
 
     public static void main(String args[]) {
 
         // Aqui declaro las variables que voy a utilizar e ingreso por teclado si hace falta
 
+        int  numero;
+        int sumatoria = 0;
+
+        System.out.println("Ingrese el numero: ");
+        numero = ingresarNumero();
 
         // Aqui agrego la logica
 
-
-
-        // Aqui  muestro el resultado final
-
+            if (numero>0); {
+                for(int i=1; i<=numero; i++) {
+                sumatoria = sumatoria + i;
+            }
+                System.out.println("La sumatoria final de " +numero+ " es: " +sumatoria);
+        }
 
     }
+
 
     public static int ingresarNumero() {
         Scanner entradaEscaner = new Scanner (System.in);
